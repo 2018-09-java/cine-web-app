f9758b2dcb40611700db5ae9daecf946c1e4776e
Simone <s.carleo@outlook.com>
2018-09-05 11:15:35 +0200

    Small changes to css and html files.

--- CinemaHTML5/public_html/code.js
+++ CinemaHTML5/public_html/code.js
@@ -5,7 +5,20 @@
  */
 var code = {
     init: function () {
-        console.log("It works!");
-    }
+        //console.log("It works!");
+        $.get("https://api.github.com/users/2018-09-java")
+                .done(code.onGetSuccessYeah)
+                .fail(code.onErrorBoooh);
+        
+    },
+    onGetSuccessYeah: function(data){
+        console.log(data);
+        $(".last-update").html("("+data.updated_at+")");
+    },
+    onErrorBoooh : function(error){
+        console.log(error);
+    },
+    
 };
+
 $(document).ready(code.init);
